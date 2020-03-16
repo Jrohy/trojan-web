e67740dc87bbea399214d3df2cfe63b7fed73f82
Jrohy <euvkzx@gmail.com>
2020-03-16 10:37:40 +0800

    精简代码

--- src/views/user/index.vue
+++ src/views/user/index.vue
@@ -304,13 +304,3 @@ export default {
     }
 }
 </script>
-
-<style>
-  .el-dropdown-link {
-    cursor: pointer;
-    color: #409EFF;
-  }
-  .el-icon-arrow-down {
-    font-size: 12px;
-  }
-</style>
