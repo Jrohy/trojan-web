06fba4bd9426358c83024558fed429b60b62f8e7
Jrohy <euvkzx@gmail.com>
2020-03-25 00:16:02 +0800

    首页加入服务器信息

--- src/store/state.js
+++ src/store/state.js
@@ -7,6 +7,7 @@ export default {
     },
     docTitle: localStorage.hasOwnProperty('docTitle') ? localStorage.getItem('docTitle') : '',
     dialogWidth: '25%',
+    nprogress: true,
     /* 导航菜单是否折叠 */
     isSidebarNavCollapse: localStorage.hasOwnProperty('isSidebarNavCollapse') ? localStorage.getItem('isSidebarNavCollapse') === 'true' : false
 }
