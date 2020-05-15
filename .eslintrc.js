c719aab4af5118333fdf12dcd299c77234e66def
Jrohy <euvkzx@gmail.com>
2020-05-15 12:55:05 +0800

    remove no-console

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,7 +8,6 @@ module.exports = {
         '@vue/standard'
     ],
     rules: {
-        'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
         'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
         'space-before-function-paren': 0,
         indent: [2, 4]
