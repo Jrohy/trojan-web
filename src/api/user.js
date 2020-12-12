5e3cca9a40797d10f74ba94e393c1f8cc8193792
Jrohy <euvkzx@gmail.com>
2020-12-12 22:22:05 +0800

    设置/取消到期时间接口

--- src/api/user.js
+++ src/api/user.js
@@ -15,3 +15,11 @@ export function updateUser(data) {
 export function delUser(id) {
     return request.delete(`/trojan/user?id=${id}`)
 }
+
+export function setExpire(data) {
+    return request.post('/trojan/user/expire', data)
+}
+
+export function cancelExpire(id) {
+    return request.delete(`/trojan/user/expire?id=${id}`)
+}
