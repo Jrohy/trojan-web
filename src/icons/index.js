ed845943c2f165e1f3a56fe3eee7e8038fdffa16
Jrohy <euvkzx@gmail.com>
2021-09-23 11:13:22 +0800

    去掉无用代码

--- src/icons/index.js
+++ src/icons/index.js
@@ -1,9 +1,3 @@
-// import Vue from 'vue'
-// import SvgIcon from '@/components/SvgIcon'// svg组件
-
-// // register globally
-// Vue.component('svg-icon', SvgIcon)
-
 const Mixins = import.meta.globEager("./svg/*.svg")
 export default {
     mixins: Object.values(Mixins).map((v) => v.default)
